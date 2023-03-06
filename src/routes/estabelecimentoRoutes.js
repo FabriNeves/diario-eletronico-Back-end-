5931646ffafb99a313acfc06bc7898efa30241aa
FaSnow <eng.fbneves@gmail.com>
2023-03-05 21:45:32 -0300

    Criação das rotas de Estabelecimento

---
+++ src/routes/estabelecimentoRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import estabelecimentoController from "../controllers/estabController.js";
+
+const estabelecimentoRouter = express.Router();
+const nomeRota = "cadastroEstab";
+
+estabelecimentoRouter.route(`/${nomeRota}`)
+  .post(estabelecimentoController.create)
+  .get(estabelecimentoController);
+
+estabelecimentoRouter.route(`/${nomeRota}/:id`)
+  .get(estabelecimentoController.readById)
+  .put(estabelecimentoController.update)
+  .delete(estabelecimentoController.delete);
+
+export default estabelecimentoRouter;
